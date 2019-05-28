c7e1c3da2ed22cea4d2ce34459cda7379d04e7d6
kuanzi <997128105@qq.com>
2019-05-28 14:51:41 +0800

    Final main.js
    Final main.js

--- main/main.js
+++ main/main.js
@@ -1,12 +1,4 @@
-// module.exports = function main() {
-//     console.log("Debug Info");
-//     return 'Hello World!';
-// };
-
 module.exports = function main(num) {
-    // var line1_component = ["._. ","... ", "._. ", "._. ", "... ", "._. ","._. ", "._. ", "._. ", "._. "];
-    // var line2_component = ["|.| ","..| ", "._| ", "._| ", "|_| ", "|_. ","|_. ", "..| ", "|_| ", "|_| "];
-    // var line3_component = ["|_| ","..| ", "|_. ", "._| ", "..| ", "._| ","|_| ", "..| ", "|_| ", "..| "];
     var line1_component = ["._.","...", "._.", "._.", "...", "._.","._.", "._.", "._.", "._."];
 	var line2_component = ["|.|","..|", "._|", "._|", "|_|", "|_.","|_.", "..|","|_|", "|_|"];
 	var line3_component = ["|_|","..|", "|_.", "._|", "..|", "._|","|_|", "..|", "|_|", "..|"];
@@ -27,21 +19,10 @@ module.exports = function main(num) {
 
 	 }
 	
- 	// line1.ToString().RTrim(' ');
   	line1.push('\n');
   	line2.push('\n');
   	line3.push('\n');
 
-  //  	line1 = line1.join('');
- 	// line2 = line2.join('');
- 	// line3 = line3.join('');
  	var res = line1.join('')+line2.join('')+line3.join('');
- 	// console.log(line1);
- 	// console.log(res);
- 	// var res=new Array();
- 	// res.push(line1,line2,line3);
- 	// res.join('');
- 	// console.log(res);
-
     return res;
 };
