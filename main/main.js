2d157029e81f3718b37b687294df4b2c815c6130
kuanzi <997128105@qq.com>
2019-05-28 14:55:26 +0800

    update format of main.js
    update format of main.js

--- main/main.js
+++ main/main.js
@@ -1,28 +1,27 @@
 module.exports = function main(num) {
-    var line1_component = ["._.","...", "._.", "._.", "...", "._.","._.", "._.", "._.", "._."];
+	var line1_component = ["._.","...", "._.", "._.", "...", "._.","._.", "._.", "._.", "._."];
 	var line2_component = ["|.|","..|", "._|", "._|", "|_|", "|_.","|_.", "..|","|_|", "|_|"];
 	var line3_component = ["|_|","..|", "|_.", "._|", "..|", "._|","|_|", "..|", "|_|", "..|"];
 	var line1=new Array();
 	var line2=new Array();
 	var line3=new Array();
-	 for (var i=0;i<num.length;i++)
-	 { 
-	     line1.push(line1_component[num[i]]);
-	     line2.push(line2_component[num[i]]);
-	     line3.push(line3_component[num[i]]);
-	     if(i != num.length - 1)
-	     {
-	     	line1.push(' ');
-		 	line2.push(' ');
-		 	line3.push(' ');
-	     }
+	for (var i=0;i<num.length;i++)
+	{
+		line1.push(line1_component[num[i]]);
+		line2.push(line2_component[num[i]]);
+		line3.push(line3_component[num[i]]);
+		if(i != num.length - 1)
+		{
+			line1.push(' ');
+			line2.push(' ');
+			line3.push(' ');
+		}
+	}
 
-	 }
-	
-  	line1.push('\n');
-  	line2.push('\n');
-  	line3.push('\n');
+	line1.push('\n');
+	line2.push('\n');
+	line3.push('\n');
 
- 	var res = line1.join('')+line2.join('')+line3.join('');
-    return res;
+	var res = line1.join('')+line2.join('')+line3.join('');
+	return res;
 };
